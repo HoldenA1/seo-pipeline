ab7cd6729eec259cf18e3df1ada77509d3267dfd
Holden Adamec <holden.adamec@gmail.com>
2025-03-19 21:23:06 -0700

    Added generated sample page

--- ssg/generate.py
+++ ssg/generate.py
@@ -1,42 +1,12 @@
 from jinja2 import Template
+import requests
+import keys
+import get_articles
 import os
 
 DIR_NAME = "website"
 
-
-def get_articles() -> list:
-    """Get a list with all the articles to generate
-
-    For now we just want to manually add articles for testing, but we'll be
-    able to customize this function to pull data from a database and later on
-    a CMS.
-    """
-
-    articles = []
-    article = {}
-    article["title"] = "Discover the Flavors of Peru at La Mar Cocina Peruana in San Francisco"
-    article["slug"] = "visit-la-mar-cocina-peruana"
-    article["content"] = """Nestled along the scenic San Francisco waterfront, La Mar Cocina Peruana offers a vibrant and authentic taste of Peru in a stunning setting. With a 4.5-star rating, this lively eatery, helmed by celebrity chef Gastón Acurio, is a must-visit for food lovers seeking fresh, flavorful seafood with a Peruvian twist.
-    A Culinary Experience by the Bay
-
-    Located right on the pier, La Mar boasts a spacious, partially outdoor dining area, making it the perfect spot to enjoy breathtaking bay views while indulging in expertly crafted dishes. Whether you're looking for a casual lunch, a romantic dinner, or a lively gathering with friends, the atmosphere is as inviting as the food itself.
-    Menu Highlights: A Celebration of Peruvian Seafood
-
-    La Mar’s menu showcases the bold and diverse flavors of Peruvian cuisine, with standouts such as:
-
-        Ceviche – Freshly prepared with zesty citrus, spice, and the perfect balance of textures.
-        Tacos – A Peruvian take on a classic favorite, packed with vibrant flavors.
-        Cooked Seafood – Expertly prepared dishes that highlight the best of the ocean’s bounty.
-
-    Why Visit La Mar?
-
-    Beyond its exceptional cuisine, La Mar is a culinary destination that brings the rich traditions of Peruvian seafood to the heart of San Francisco. Whether you’re a ceviche connoisseur or a first-time explorer of Peruvian flavors, this restaurant delivers a dining experience that is both authentic and unforgettable.
-
-    Ready to embark on a flavorful journey? Visit La Mar Cocina Peruana to explore the menu and make your reservation today!\n"""
-    articles.append(article)
-    return articles
-
-articles = get_articles()
+articles = get_articles.articles
 
 # Load Jinja2 template
 with open("template.html") as f:
@@ -47,16 +17,25 @@ os.makedirs("website", exist_ok=True)
 
 # Generate HTML files
 for article in articles:
-    title = article["title"]
-    slug = article["slug"]
-    paragraphs = article["content"].split('\n')
-    seo = article.get("seoMetaDescription", "")
+    info = '</p><p>'.join(article["DetailedInformation"].split('\n'))
 
-    content = "</p><p>".join(paragraphs)
+    html_content = template.render(
+        seo=article["SEOMetaDescription"],
+        title=article["Title"],
+        placeName=article["PlaceName"],
+        generalSummary=article["Summary"],
+        rating=article["Rating"],
+        reviewsSummary=article["ReviewsSummary"],
+        reviews=article["SampleReviews"],
+        detailedInformation=info,
+        formattedAddress=article["FormattedAddress"],
+        locationWebsite=article["WebsiteURL"]
+    )
 
-    html_content = template.render(title=title, content=content, seo=seo)
-
-    with open(f"{DIR_NAME}/{slug}.html", "w") as f:
+    with open(f"{DIR_NAME}/{article["Slug"]}.html", "w") as f:
         f.write(html_content)
+    
+    print(f"Made page {article["Slug"]}")
+
 
 print(f"Static site generated in '{DIR_NAME}/' directory!")
---
+++ ssg/get_articles.py
@@ -0,0 +1,84 @@
+articles = [
+    {
+        "Slug": "pacific-cocktail-haven-san-francisco",
+        "SEOMetaDescription": "Experience the artistry of mixology at Pacific Cocktail Haven, a stylish bar in San Francisco with creative cocktails, lively ambiance, and festive decor.",
+        "Title": "Pacific Cocktail Haven: San Francisco’s Premier Spot for Unique Cocktails and Vibrant Gatherings",
+        "PlaceName": "Pacific Cocktail Haven",
+        "Summary": "Pacific Cocktail Haven is a must-visit destination for cocktail enthusiasts and social gatherings. Known for its inventive drinks, lively vibe, and seasonal decor, this bar offers something special for every occasion. Whether you're indulging in their Thai-inspired cocktails or enjoying the festive holiday ambiance, PCH guarantees a memorable experience.",
+        "Rating": "4.6",
+        "ReviewsSummary": "Guests rave about Pacific Cocktail Haven’s creative and delicious cocktails, friendly staff, and vibrant atmosphere. Positive reviews highlight unique drinks like the Thai iced tea cocktail and espresso martini, as well as the holiday-themed decor during Christmas. Negative feedback mentions long wait times, crowded spaces, and high prices for drinks.",
+        "SampleReviews": [
+        {
+            "authorName": "Hilda Liu",
+            "authorProfileURL": "https://www.google.com/maps/contrib/110795690159586273067/reviews",
+            "authorPhotoURL": "https://lh3.googleusercontent.com/a-/ALV-UjX5Zy8aoxc9ydB_KKyCXG-ulmausugJPzBS8MNY8KI1DKn3m51wHg=s128-c0x00000000-cc-rp-mo-ba6",
+            "rating": "5.0",
+            "timePublished": "2025-02-28 11:21:24.557870+00:00",
+            "review": "I have heard of this place for a long time. They were so crowded on a weekend night and we couldn’t really find a seat. We got lucky and someone left so we just grabbed the spot. The cocktails were very unique. It’s one of the best bar I have been. They don’t have any food to serve on the side. It tells you how much confident they have for their cocktail. The environment is very chill. It’s definitely not quiet, but not loud enough that you cannot hear other people. They are also very professional. I would recommend here if you wanna try unique cocktails."
+        },
+        {
+            "authorName": "Katara Zai",
+            "authorProfileURL": "https://www.google.com/maps/contrib/114816319639344928204/reviews",
+            "authorPhotoURL": "https://lh3.googleusercontent.com/a/ACg8ocKC_eS1dXEPLsQR-s5_4ru6l72RNtMMS13s2oWIQT2cP_RKOw=s128-c0x00000000-cc-rp-mo-ba2",
+            "rating": "5.0",
+            "timePublished": "2024-09-19 00:49:26.321153+00:00",
+            "review": "such a cute chill vibe here! visiting from out of town & had some of the best drinks here. Our host was super nice & wrote down a list of bars for a bar crawl on our way to a concert. Waitress was also super sweet & nice. 100% recommend stopping by.\n\nThe drinks were SO yummy. I got a virgin version of their Thai iced tea cocktail (forgot the name) & their espresso martini was also delicious. 10/10 overall."
+        },
+        {
+            "authorName": "Corina",
+            "authorProfileURL": "https://www.google.com/maps/contrib/116004891776200542727/reviews",
+            "authorPhotoURL": "https://lh3.googleusercontent.com/a-/ALV-UjV6e-1-9sQF3-YZbBc2Yi6yUIGE06a1lsS3fA_HhE1hAinuw4nROQ=s128-c0x00000000-cc-rp-mo-ba5",
+            "rating": "5.0",
+            "timePublished": "2024-11-10 09:19:15.439691+00:00",
+            "review": "This place recently became my favorite cocktail bar in SF. The drinks are creative, visually stunning, and delicious. All the staff members I’ve interacted with here are super friendly. On my second visit to PCH during a girls’ night out, Chelsea took care of my group, introduced herself after I said I recognized her from when I first came here during Halloweekend, and took the time to get to know us. My experience based on both visits here is 10/10. This place is popular for a reason, so you’ll definitely want to hit up this spot for your next night out!"
+        },
+        {
+            "authorName": "Michelle",
+            "authorProfileURL": "https://www.google.com/maps/contrib/115040615367470041750/reviews",
+            "authorPhotoURL": "https://lh3.googleusercontent.com/a/ACg8ocK5QEfS-jShKsiO6Nj8IUGUD9XUlYBkoOGxhRkrqVM2MHuflA=s128-c0x00000000-cc-rp-mo-ba4",
+            "rating": "4.0",
+            "timePublished": "2024-11-09 21:27:43.600821+00:00",
+            "review": "Came here for drinks with some colleagues! It\'s honestly a great venue for friends gathering or work event. They do not take reservations - maybe for private events they do, but the place was big enough to accommodate walk-ins on weekday nights. They have outdoor seating as well as indoors - we chose the outdoor seating at the time cause we wanted some fresh air (beware though, it got pretty chilly later in the night but we were not offered a seat inside so that was the slight downside).\n\nService was good - the waiter was attentive and provided good recommendations, while also not too aggressively checking in with us - which helped us keep our conversations going.\n\nIn terms of drinks, we ordered two rounds of drinks, and the most recommended drink would be the Pornstar Martini (Reyka Vodka, passionfruit, vanilla, citrus, sparkline wine) - it was great presentation and super delicious - citrusy and sweet. The second recommendation would be the All Thai’d up (El Jolgorio Tepeztate, rum, lime leaf, coconut, tom yum, citrus, thai chili) - don\'t get scared off by the name, the tom yum taste is very slight, so it\'s a super unique and interesting drink that\'s very tasty. Last recommendation is the Golden Opportunity (Sherry, blanc vermouth, golden sesame, macadamia nut, cointreau, cacao) - this drink has a very interesting before, during, and after taste - very nutty finish, definitely recommend if you like sherry!\n\nDefinitely looking to come back again!"
+        },
+        {
+            "authorName": "Wilson Tsang",
+            "authorProfileURL": "https://www.google.com/maps/contrib/100618295330470233704/reviews",
+            "authorPhotoURL": "https://lh3.googleusercontent.com/a-/ALV-UjW1kTgPDOvyjwEg_exTOd6lD0ZU8qSEafa8oISeeXTiW-7EcdxM=s128-c0x00000000-cc-rp-mo-ba7",
+            "rating": "3.0",
+            "timePublished": "2024-12-24 19:59:16.560291+00:00",
+            "review": "TLDR: you’re paying for the atmosphere mainly\n\nThe atmosphere is very nice as it is heavily decorated for Christmas, as well as having holiday themed drinks. But with a $10 cover and $20 drinks that are fairly small cocktails, you’re paying about $40 for one drink after tax and tip. That just doesn’t seem justified. On top of that, it gets very stuffy inside, circulation is not very good"
+        }],
+        "DetailedInformation": """Pacific Cocktail Haven (PCH) stands out as one of San Francisco’s top destinations for craft cocktails and social gatherings. Situated in the heart of the city at 550 Sutter Street, this establishment combines innovative mixology with a lively ambiance that’s perfect for catching up with friends or celebrating special occasions.
+Creative Cocktails
+
+PCH is renowned for its unique drink offerings that push the boundaries of traditional mixology. Highlights from their menu include:
+
+    Pornstar Martini: A blend of passionfruit, vanilla, citrus, and sparkling wine with a stunning presentation.
+
+    All Thai’d Up: A bold fusion of rum, lime leaf, coconut, tom yum spices, and Thai chili for an exotic twist.
+
+    Golden Opportunity: A nutty concoction featuring sherry, vermouth, sesame, macadamia nut, cacao, and cointreau.
+
+For those seeking non-alcoholic options, mocktails like the virgin Thai iced tea cocktail are equally delightful.
+Festive Ambiance
+
+One of PCH’s defining features is its seasonal decor. During the holidays, the bar transforms into a winter wonderland with dazzling lights, themed drinks served in collectible mugs, and even occasional snow machine effects. This festive atmosphere makes it a popular destination during Christmas time.
+Perfect for Groups
+
+The bar’s layout includes both indoor seating and an outdoor patio area. While seating operates on a first-come-first-serve basis (no reservations), the spacious venue can accommodate groups comfortably on less crowded nights. The attentive staff adds to the experience by providing personalized drink recommendations and ensuring guests feel welcome.
+Things to Consider
+
+While PCH offers an unforgettable experience, it’s important to plan ahead:
+
+    Arrive early to avoid long wait times during peak hours.
+
+    Be prepared for premium pricing; cocktails average $20 each.
+
+    Expect a lively atmosphere that may be noisy when crowded.
+
+Whether you’re looking to explore creative cocktails or enjoy a festive night out with loved ones, Pacific Cocktail Haven delivers an exceptional experience that blends artistry and ambiance seamlessly.
+""",
+        "FormattedAddress": "550 Sutter St, San Francisco, CA 94108, USA",
+        "WebsiteURL": "www.pacificcocktailsf.com"
+    }
+]
