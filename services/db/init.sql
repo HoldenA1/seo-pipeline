89edaa2d1972050df34fa58b60ce294d9acab7cd
Holden Adamec <holden.adamec@gmail.com>
2025-04-14 17:11:04 -0700

    Fixed database config

--- services/db/init.sql
+++ services/db/init.sql
@@ -1,27 +1,26 @@
--- Create DBs for strapi and another service
-CREATE DATABASE strapi_db;
-CREATE DATABASE aidb;
+CREATE TABLE IF NOT EXISTS places (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    address TEXT,
+    rating REAL,
+    reviews_count REAL,
+    editorial_summary TEXT,
+    business_site TEXT,
+    city TEXT,
+    state TEXT,
+    country TEXT,
+    types TEXT,
+    primary_type TEXT,
+    status INT
+);
 
--- Create tables for services
--- CREATE TABLE IF NOT EXISTS places (
---     id TEXT PRIMARY KEY,
---     name TEXT,
---     address TEXT,
---     rating REAL,
---     reviews_count REAL,
---     editorial_summary TEXT,
---     business_site TEXT,
---     city TEXT,
---     status INT
--- );
-
--- CREATE TABLE IF NOT EXISTS reviews (
---     id SERIAL PRIMARY KEY,
---     place_id TEXT REFERENCES places(id) ON DELETE CASCADE,
---     author_name TEXT,
---     author_uri TEXT,
---     author_photo TEXT,
---     rating REAL,
---     publish_time TEXT,
---     review_text TEXT
--- );
+CREATE TABLE IF NOT EXISTS reviews (
+    id SERIAL PRIMARY KEY,
+    place_id TEXT REFERENCES places(id) ON DELETE CASCADE,
+    author_name TEXT,
+    author_uri TEXT,
+    author_photo TEXT,
+    rating REAL,
+    publish_time TEXT,
+    review_text TEXT
+);
